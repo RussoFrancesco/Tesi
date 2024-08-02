64e766c471167b8698786fa24c272eff58288c23
Francesco Russo <franci.russo@icloud.com>
2024-08-02 18:44:21 +0200

    SmolLM

---
+++ SmolLM/model.py
@@ -0,0 +1,73 @@
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+import time
+import psutil
+import os
+import torch
+from evaluate import load
+import nltk
+from nltk.translate.bleu_score import sentence_bleu
+import sys
+
+
+percorso_progetto = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+
+if percorso_progetto not in sys.path:
+    sys.path.append(percorso_progetto)
+
+from hallucination import calculate_hallucination
+from write_on_file import write_on_file
+
+model_params = sys.argv[1]
+
+model_path = f'HuggingFaceTB/SmolLM-{model_params}'
+filename = f'SmolLM-{model_params}.csv'
+
+nltk.download('punkt')
+
+
+perplexity_metric = load("perplexity", module_type="metric")
+
+def calculate_perplexity(text):
+    results = perplexity_metric.compute(predictions=[text], model_id=model_path)
+    print(results)
+    return results['mean_perplexity']
+
+def calculate_bleu(reference, text):
+    reference_tokens = nltk.word_tokenize(reference)
+    text_tokens = nltk.word_tokenize(text)
+    bleu = sentence_bleu([reference_tokens], text_tokens)
+    return bleu
+
+
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16)
+print(f"Memory footprint: {model.get_memory_footprint() / 1e6:.2f} MB")
+
+generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
+
+dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split='test')
+texts = dataset['text']
+
+for i, input_text in enumerate(texts):
+        
+    cpu_usage_before = psutil.cpu_percent(interval=1)
+    memory_usage_before = psutil.virtual_memory().used
+    num_tokens = tokenizer(input_text, return_tensors="pt").input_ids.shape[-1]
+
+    start_time = time.time()
+    generated_text = generator(input_text, max_new_tokens=100, num_return_sequences=1)[0]['generated_text']
+    end_time = time.time()
+
+    cpu_usage_after = psutil.cpu_percent(interval=1)
+    memory_usage_after = psutil.virtual_memory().used
+
+    inference_time = end_time - start_time
+
+    perplexity = calculate_perplexity(generated_text)
+    bleu = calculate_bleu(input_text, generated_text)
+    hallucination = calculate_hallucination(input_text, generated_text)
+
+
+    write_on_file(filename, i, num_tokens, inference_time, cpu_usage_before, cpu_usage_after, memory_usage_before, memory_usage_after, perplexity, bleu, hallucination)
