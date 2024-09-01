fd33f110ab98f061b728571b24bf81953029217d
Francesco Russo <franci.russo@icloud.com>
2024-09-01 11:35:26 +0200

    modifiche SmolLM

--- test.sh
+++ test.sh
@@ -5,12 +5,12 @@
 #python3 model.py 1.3B
 #cd ..
 cd SmolLM
-python3 model.py 135M
+#python3 model.py 135M
 python3 model.py 360M
 python3 model.py 1.7B
-cd ..
-cd tinyLLaMA
-python3 model.py
+#cd ..
+#cd tinyLLaMA
+#python3 model.py
 #cd ..
 #cd mobileLLaMA
 #python3.10 model.py
