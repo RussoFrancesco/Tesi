4da314f7bed33e7f7fbee63068407d7c68fb7c46
Francesco Russo <franci.russo@icloud.com>
2024-09-09 08:29:53 +0200

    test.sh per mac e primi file xls

--- test.sh
+++ test.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-#cd gpt-neo
-#python3 model.py 125M
-#python3 model.py 1.3B
-#cd ..
+cd gpt-neo
+python3.10 model.py 125M
+python3.10 model.py 1.3B
+python3.10 model.py 2.7B
+cd ..
 cd SmolLM
-#python3 model.py 135M
-python3 model.py 360M
-python3 model.py 1.7B
-#cd ..
-#cd tinyLLaMA
-#python3 model.py
-#cd ..
-#cd mobileLLaMA
-#python3.10 model.py
-#cd ..
-#cd Gemma
-#python3.10 model.py
+python3.10 model.py 135M
+python3.10 model.py 360M
+python3.10 model.py 1.7B
+cd ..
+cd tinyLLaMA
+python3.10 model.py
+cd ..
+cd mobileLLaMA
+python3.10 model.py
+cd ..
+cd Gemma
+python3.10 model.py
 
