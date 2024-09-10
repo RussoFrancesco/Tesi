d7785306820d08eda884ffcfc3eda559c9cb8796
Francesco Russo <franci.russo@icloud.com>
2024-09-10 09:33:53 +0200

    risultati da mac

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd gpt-neo
-python3.10 model.py 125M
-python3.10 model.py 1.3B
-python3.10 model.py 2.7B
-cd ..
-cd SmolLM
-python3.10 model.py 135M
-python3.10 model.py 360M
-python3.10 model.py 1.7B
-cd ..
+#cd gpt-neo
+#python3.10 model.py 125M
+#python3.10 model.py 1.3B
+#python3.10 model.py 2.7B
+#cd ..
+#cd SmolLM
+#python3.10 model.py 135M
+#python3.10 model.py 360M
+#python3.10 model.py 1.7B
+#cd ..
 cd tinyLLaMA
 python3.10 model.py
 cd ..
