cc6db0eb11fd6bcc2d0e04ca3206866932ed5041
Francesco Russo <franci.russo@icloud.com>
2024-09-11 17:01:07 +0200

    altri grafici

--- plot.py
+++ plot.py
@@ -5,15 +5,15 @@ import pandas as pd
 # Funzione per caricare i dati e calcolare il tempo medio di inferenza
 def load_and_calculate_mean_time(file_path):
     df = pd.read_csv(file_path).head(100)
-    return np.mean(df['Tempo di inferenza'])
+    return np.mean(df['Perplexity'])
 
 # File paths e labels
 file_paths = [
-    "gpt-neo/gpt-neo-125M-raspberry.csv",
-    "gpt-neo/gpt-neo-1.3B-raspberry.csv"
+    "SmolLM/SmolLM-1.7B-raspberry.csv",
+    "Gemma/Gemma-2b-mac.csv"
 ]
 
-labels = ["Gpt-neo-125M", "Gpt-neo-1.3B"]
+labels = ["SmolLM-1.7B", "Gemma-2b"]
 
 # Calcola i tempi medi per tutti i file
 mean_times = [load_and_calculate_mean_time(fp) for fp in file_paths]
@@ -28,8 +28,8 @@ bars = ax.bar(x, mean_times, width)
 
 # Aggiungi etichette e titolo
 ax.set_xlabel('Modello')
-ax.set_ylabel('Tempo medio di inferenza(s)')
-ax.set_title('Confronto dimensioni Gpt-neo su Raspberry Pi')
+ax.set_ylabel('Perplexity media')
+ax.set_title('Confronto perplexity media SmolLM e Gemma')
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
 
@@ -41,7 +41,10 @@ for bar in bars:
                 xytext=(0, 3),  # 3 punti di offset verticale
                 textcoords="offset points",
                 ha='center', va='bottom')
+    
 
 # Mostra il grafico
 plt.show()
+
+
 
